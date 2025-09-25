164cb5aa48cede13a9b1a902ebcc03d4abfabfde
CK <cklau00@gmail.com>
2025-09-25 00:59:14 -0400

    fix pylint issue on server.py

--- server.py
+++ server.py
@@ -17,9 +17,9 @@ def detect_emotion():
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
 
-    if response['dominant_emotion'] == None:
+    if not response['dominant_emotion']:
         return "Invalid text! Please try again!"
-    
+
     result = f"""
     For the given statement, the system response is 'anger': {response['anger']},
     'digust': {response['disgust']}, 'fear': {response['fear']}, 
