f6383a982d32940fc4d761eb448261cd2e92f653
CK <cklau00@gmail.com>
2025-09-24 23:22:07 -0400

    Update emotion_detector to return a dict response

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,16 +2,17 @@
 A simple module to perform emotion analysis using IBM Watsion AI
 
 """
+import json
 import requests
 
-def emotion_detector(text_to_analyse: str):
+def emotion_detector(text_to_analyse: str) -> dict:
     """
     Perform emotion analysis on incoming text
     :arg
         text_to_analyse (str): incoming text
 
     :return
-        the result
+        the emotion metrics in a dictionary
     """
     ibm_url = "https://sn-watson-emotion.labs.skills.network/v1"
     watson_uri = "watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -20,5 +21,19 @@ def emotion_detector(text_to_analyse: str):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header, timeout=300)
+    formatted_response = json.loads(response.text)
 
-    return response.text
+    emotion_response = formatted_response['emotionPredictions'][0]['emotion']
+
+    emotion_response = {
+        'anger': float(emotion_response["anger"]),
+        'disgust': float(emotion_response["disgust"]),
+        'fear': float(emotion_response["disgust"]),
+        'joy': float(emotion_response["joy"]),
+        'sadness': float(emotion_response["sadness"]),            
+    }
+    sorted_emotion_list = sorted(emotion_response.items(), key=lambda item: item[1], reverse=True)
+    dominant_emotion = sorted_emotion_list[0][0]
+    emotion_response['dominant_emotion'] = dominant_emotion
+
+    return emotion_response
